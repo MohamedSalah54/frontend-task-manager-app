8c5fc1a90577dc09c4c74c0ba0e324c88182f6d6
MohamedSalah54 <mohamed.salah199988.com>
2025-10-12 15:27:47 +0300

    config next

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['localhost'],  // إضافة localhost هنا للسماح بتحميل الصور
+    domains: [
+      'localhost',
+      'https://backend-task-manager-app-production.up.railway.app'
+    ],
     remotePatterns: [
       {
         protocol: 'http',
         hostname: 'localhost',
         port: '3001',
-        pathname: '/static/uploads/**', // مسار صحيح للصور
+        pathname: '/static/uploads/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'https://backend-task-manager-app-production.up.railway.app/',
+        pathname: '/static/uploads/**',
       },
     ],
   },
